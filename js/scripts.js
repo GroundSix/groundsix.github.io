77bbe507293346c7c9a992c12c984b97ef7231bd
David Haigh <david.haigh@groundsix.com>
2014-01-30 17:17:10 +0000

    [In progress: 4] Main menu fix

--- js/scripts.js
+++ js/scripts.js
@@ -6,79 +6,79 @@ $(window).scroll(function() {
 // Nav scroll to #anchor
 $(function() {
     if ($(window).width() >= 1024) {
-        $('#home').click(function(e) {
+        $('.nav-home').click(function(e) {
             e.preventDefault();
             $('html, body').animate({
                 scrollTop: $('#panel-one').offset().top
             }, 1500);
         });
-        $('#businesses').click(function(e) {
+        $('.nav-businesses').click(function(e) {
             e.preventDefault();
             $('html, body').animate({
                 scrollTop: $('#panel-two').offset().top - 86
             }, 1500);
         });
-        $('#ideas-anchor').click(function(e) {
+        $('.nav-ideas-anchor').click(function(e) {
             e.preventDefault();
             $('html, body').animate({
                 scrollTop: $('#panel-three').offset().top - 86
             }, 1500);
         });
-        $('#the-team').click(function(e) {
+        $('.nav-the-team').click(function(e) {
             e.preventDefault();
             $('html, body').animate({
                 scrollTop: $('#panel-four').offset().top - 90
             }, 1500);
         });
-        $('#g6-life').click(function(e) {
+        $('.nav-g6-life').click(function(e) {
             e.preventDefault();
             $('html, body').animate({
                 scrollTop: $('#panel-five').offset().top - 86
             }, 1500);
         });
-        $('#contact').click(function(e) {
+        $('.nav-contact').click(function(e) {
             e.preventDefault();
             $('html, body').animate({
                 scrollTop: $('#panel-six').offset().top - 90
             }, 1500);
         });
     } else {
-        $('#home').click(function(e) {
+        $('.nav-home').click(function(e) {
             e.preventDefault();
             $('html, body').animate({
                 scrollTop: $('#panel-one').offset().top
             }, 1500);
             $('.top-bar').removeClass('expanded');
         });
-        $('#businesses').click(function(e) {
+        $('.nav-businesses').click(function(e) {
             e.preventDefault();
             $('html, body').animate({
                 scrollTop: $('#panel-two').offset().top - 40
             }, 1500);
             $('.top-bar').removeClass('expanded');
         });
-        $('#ideas-anchor').click(function(e) {
+        $('.nav-ideas-anchor').click(function(e) {
             e.preventDefault();
             $('html, body').animate({
                 scrollTop: $('#panel-three').offset().top - 40
             }, 1500);
             $('.top-bar').removeClass('expanded');
         });
-        $('#the-team').click(function(e) {
+        $('.nav-the-team').click(function(e) {
             e.preventDefault();
             $('html, body').animate({
                 scrollTop: $('#panel-four').offset().top - 44
             }, 1500);
             $('.top-bar').removeClass('expanded');
         });
-        $('#g6-life').click(function(e) {
+        $('.nav-g6-life').click(function(e) {
             e.preventDefault();
             $('html, body').animate({
                 scrollTop: $('#panel-five').offset().top - 40
             }, 1500);
             $('.top-bar').removeClass('expanded');
         });
-        $('#contact').click(function(e) {
+        $('.nav-contact').click(function(e) {
             e.preventDefault();
             $('html, body').animate({
                 scrollTop: $('#panel-six').offset().top - 44
